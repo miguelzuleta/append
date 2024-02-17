f02f2c598ad8302ad843c8d03bcd316466b155c0
miguelzuleta <8463388+miguelzuleta@users.noreply.github.com>
2024-02-16 16:02:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,154 +2,88 @@ if (window.appended === undefined) {
 	window.appended = {};
 }
 
-const append = props => {
-	let svgElems = ['animate', 'animateMotion', 'animateTransform', 'circle', 'clipPath', 'color-profile', 'defs', 'desc', 'discard', 'ellipse', 'feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite', 'feConvolveMatrix', 'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight', 'feDropShadow', 'feFlood', 'feFuncA', 'feFuncB', 'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology', 'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotLight', 'feTile', 'feTurbulence', 'filter', 'foreignObject', 'g', 'hatch', 'hatchpath', 'image', 'line', 'linearGradient', 'marker', 'mask', 'mesh', 'meshgradient', 'meshpatch', 'meshrow', 'metadata', 'mpath', 'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect', 'set', 'solidcolor', 'stop', 'svg', 'switch', 'symbol', 'text', 'textPath', 'title', 'tspan', 'unknown', 'use', 'view'];
-	let { parent, child, update, label } = props;
-	let dataAttr = 'data-current-elem';
-	let parentElems = [];
-	let parentRender = false;
-
-	if (typeof parent == 'object') {
-		parentElems = Object.keys(parent).length > 1 ? [...parent] : [parent];
-	} else {
-		parentElems = [...document.querySelectorAll(parent)];
-	}
-	
-	if ((label && appended[label] === undefined) || (label && appended[label] && update)) {
-		appended[label] = {
-			parent: false
-		};
+const append = (renderParent, htmlStr, renderType ) => {
+	let parentSelector = (typeof renderParent === 'string') ? document.querySelector(renderParent) : renderParent;
+				
+	if (renderType == 'update') {
+		parentSelector.innerHTML = '';
 	}
 
-	parentElems.forEach((parentEl, index) => {
-		if (update) {
-			parentElems[index].innerHTML = '';
-			parentRender = {
-				parent: parentElems[index]
-			};
+	let parentAttr = 'bb-parent-label';
+	let groupAttr = 'bb-group';
+	let labelAttr = 'bb-label';
 
-			if (child && 'elem' in child[0]) {
-				child = [{ child: child }]
+	let appendedObj = appended;
+	let parsedHTML = new DOMParser().parseFromString(htmlStr, 'text/html');
+	let parsedBody = parsedHTML.querySelector('body');
+	let setParsedElements = [...parsedBody.children];
+
+	let addAppendedElements = rootEl => {
+		let groupAttrNames = () => rootEl.getAttribute(groupAttr).split(' ');
+
+		let setGroupNameValue = (name, value) => {
+			if (appendedObj[name] === undefined || (appendedObj[name] && renderType == 'update')) {
+				appendedObj[name] = value;
 			}
 		}
-		
-		if (child) {
-			child.forEach(childEl => {
-				let newElem = {};
 
-				if (childEl.elem) {
-					if (svgElems.includes(childEl.elem)) {
-						newElem = document.createElementNS('http://www.w3.org/2000/svg', childEl.elem);
-					} else {
-						newElem = document.createElement(childEl.elem);
-					}
-				} else {
-					newElem = document.createElement('div');
-				}
-				
-				if (parentRender && !parentRender.child) {
-					parentRender['child'] = newElem;
-				}
+		if (rootEl.hasAttribute(labelAttr)) {
+			if (rootEl.hasAttribute(groupAttr)) {
+				groupAttrNames().forEach(groupName => {
+					setGroupNameValue(groupName, {});
 
-				newElem.setAttribute(dataAttr, '');
+					appendedObj[groupName][rootEl.getAttribute(labelAttr)] = rootEl;
+				})
 
-				if (childEl.class) {
-					newElem.setAttribute('class', childEl.class);
-				}
+			} else {
+				appendedObj[rootEl.getAttribute(labelAttr)] = rootEl;
+			}
+		} else if (rootEl.hasAttribute(groupAttr)) {
+			groupAttrNames().forEach(groupName => {
+				setGroupNameValue(groupName, []);
 
-				if (childEl.id) {
-					newElem.id = childEl.id;
-				}
-
-				if (childEl.text) {
-					newElem.innerHTML = childEl.text;
-				}
-
-				for (let attrKey in childEl.attrs) {
-					newElem.setAttribute(attrKey, childEl.attrs[attrKey]);
-				}
-
-				parentEl.appendChild(newElem);
-				parentEl.removeAttribute(dataAttr);
-				
-				if (appended[label] && !appended[label]['parent']) {
-					appended[label]['parent'] = parentEl;
-				}
-				
-				let groupUndefined = val => appended[label][val] === undefined;
-				let labelError = prop => {
-					if (childEl[prop] && !label) {
-						throw new Error(`To use { ${prop}: '${childEl[prop]}' } you need to set a label prop at the parent level of the object.`);
-					}
-				}
-
-				labelError('label');
-				labelError('labelGroup');
-				
-				let splitLabel = null;
-				
-				if (childEl.labelGroup) {
-					splitLabel = childEl.labelGroup.split(' ');
-				}
-				
-				if (childEl.label) {
-					if (childEl.labelGroup) {
-						if (splitLabel.length > 1) {
-							splitLabel.forEach(item => {
-								if (groupUndefined(item)) {
-									appended[label][item] = {};
-								}
-
-								appended[label][item][childEl.label] = newElem;
-							})
-						} else {
-							if (groupUndefined(childEl.labelGroup)) {
-								appended[label][childEl.labelGroup] = {};
-							}
-
-							appended[label][childEl.labelGroup][childEl.label] = newElem;
-						}
-					} else {
-						appended[label][childEl.label] = newElem;
-					}
-				} else {
-					if (childEl.labelGroup) {
-						if (splitLabel.length > 1) {
-							splitLabel.forEach(item => {
-								if (groupUndefined(item)) {
-									appended[label][item] = [];
-								}
-
-								appended[label][item].push(newElem);
-							})
-						} else {
-							if (groupUndefined(childEl.labelGroup)) {
-								appended[label][childEl.labelGroup] = [];
-							}
-
-							appended[label][childEl.labelGroup].push(newElem);
-						}
-					}
-				}
-
-				if (childEl['child'] && (typeof childEl['child'] === 'object')) {
-					append({
-						parent: `[${dataAttr}]`,
-						child: childEl['child'],
-						...({ label } || {})
-					})
-				} else {
-					newElem.removeAttribute(dataAttr);
-				}
+				appendedObj[groupName].push(rootEl);
 			})
 		}
-		
-		if (parentRender) {
-		 [...parentRender['child'].childNodes].forEach(node => {
-			parentRender['parent'].appendChild(node);
-		 })
+	}
 
-		 parentRender['parent'].childNodes[0].remove();
+	if (parsedBody.innerText !== '' && !setParsedElements.length) {
+		setParsedElements = parsedBody.innerText;
+
+		if (renderType == 'update') {
+			parentSelector.innerText = setParsedElements; 
+		} else {
+			parentSelector.innerText += setParsedElements;
 		}
-	})  
+	} else {
+		setParsedElements.forEach((el, index) => {
+			if (el.hasAttribute(parentAttr)) {
+				let parentAttrName = el.getAttribute(parentAttr);
+
+				if (appendedObj[parentAttrName] === undefined) {
+					appendedObj[parentAttrName] = {};	
+				}
+
+				appendedObj = appendedObj[parentAttrName];
+			}
+
+			addAppendedElements(el);
+
+			let childLabels = [...el.querySelectorAll('[bb-group], [bb-label]')];
+
+			childLabels.forEach(label => {
+				addAppendedElements(label);
+			});
+
+			if (renderType === 'prepend') {
+				if (index === 0) {
+					parentSelector.prepend(el);
+				} else {
+					setParsedElements[index - 1].after(el);
+				}
+			} else {
+				parentSelector.appendChild(el);
+			}
+		})
+	}
 }
